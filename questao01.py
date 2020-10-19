29c23e6579c2b4e0ca7638d145877000645daab8
Jonathaft <jonathaft@gmail.com>
2020-10-19 11:01:04 -0300

    Add files via upload

--- questao01.py
+++ questao01.py
@@ -1,6 +1,13 @@
 import scrapy
 
 class UolSpider(scrapy.Spider):
+    """
+    Classe que coleta a cotação do dolar no site da UOL 
+    Para executar :
+        - scrapy runspider questao01.py --nolog
+        ou
+        - scrapy runspider questao01.py -s LOG_ENABLED=False
+    """
     name = 'uol_spider'
     start_urls = ['https://www.uol.com.br']
     
